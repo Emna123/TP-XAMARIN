346734c2acb2f80da62f6772210dc53a363731d7
Emna123 <emnatijani77@gmail.com>
2020-12-28 21:03:13 +0100

    TP

---
+++ Tp/Tp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Tp.views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using NavigationPage = Xamarin.Forms.NavigationPage;
+
+namespace Tp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new Login());
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Tp/Tp/views/Navigation.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tp.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Navigation : ContentPage
+    {
+        public Navigation()
+        {
+            InitializeComponent();
+        }
+       private void OnClick1(object sender, EventArgs e)
+        {
+
+            Navigation.PushAsync(new MasterDetailPage());
+        }
+       private void OnClick2(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new NavigationPage());
+        }
+        private void OnClick3(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new TabbedPage());
+        }
+        private void OnClick4(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CarouselPage());
+        }
+    }
+}
